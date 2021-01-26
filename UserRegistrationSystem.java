f87723a66f6bdae6555097d9d62b8d8d379271ad
Bhushan Patil <bhupatil0001@gmail.com>
2021-01-26 11:24:19 +0530

    ADDED: validation check for email

--- UserRegistrationSystem.java
+++ UserRegistrationSystem.java
@@ -1,11 +1,10 @@
+
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-//UC2
-public class UserRegistrationSystem  {
-
+public class UserRegistrationSystem {
 	static Scanner sc = new Scanner(System.in); // declare static
 
 	void fNameCheck() { // first name check
@@ -33,7 +32,7 @@ public class UserRegistrationSystem  {
 		System.out.println("Enter your last name: ");
 		String name = sc.nextLine();
 
-		String regex = "^[A-Z][a-z]{2,}$"; // first name validation check condition
+		String regex = "^[A-Z][a-z]{2,}$"; // last name validation check
 		// Creating a pattern object
 		Pattern pattern = Pattern.compile(regex);
 		// Creating a Matcher object
@@ -48,21 +47,46 @@ public class UserRegistrationSystem  {
 		}
 	}
 
+	void validEmailCheck() {
+		System.out.println("Enter your email id: ");
+		String name = sc.nextLine();
+
+		String regex = "[a-z]+[.]+[a-z]+[@]+[a-z]{1,2}+[.]+[a-z]{1,2}+[.]+[a-z]{1,2}$";
+
+		// Creating a pattern object
+		Pattern pattern = Pattern.compile(regex);
+		// Creating a Matcher object
+		Matcher matcher = pattern.matcher(name);
+		// Verifying whether given phone number is valid
+		boolean result = matcher.matches();
+		if (result == true) {
+			System.out.println(" Email is valid");
+		} else {
+			System.out.println("Not valid");
+			System.out.println("Email id shhoul be this pattern ex:abc.xyz@bl.co.in ");
+		}
+	}
+
 	public static void main(String[] args) throws IOException {
 
-		UserRegistrationSystem  ob = new UserRegistrationSystem ();
+		UserRegistrationSystem ob = new UserRegistrationSystem();
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("1.First Name Validation");
 		System.out.println("2.Last Name Validation");
-		System.out.print("Enter choice");
+		System.out.println("3.Email Validation ");
+		System.out.println("Enter choice");
 
 		int choice = sc.nextInt();
 		switch (choice) {
 		case 1:
 			ob.fNameCheck();
-			break;
+			break;									//METHOD CALLING
 		case 2:
 			ob.lNameCheck();
+			break;
+		case 3:
+			ob.validEmailCheck();
 			break;
 		}
 	}
